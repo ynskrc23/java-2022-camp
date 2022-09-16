38513dc23665900d8067a7e8fa3f92a308e88f26
Yunus Karaca <ynskrc82@gmail.com>
2022-09-17 01:01:44 +0300

    first lesson commit

---
+++ src/asalSayi/Main.java
@@ -0,0 +1,14 @@
+package asalSayi;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+
+        String[] krediler = {"Hızlı Kredi", "Maaşını Halkbanktan", "Emekli"};
+
+        for (int i = 0; i < krediler.length; i++) {
+            System.out.println(krediler[i]);
+        }
+    }
+}
